d0dc9163712c0a20f0a3270f752a38a6731a02aa
Goldy T Joseph <35062235+gol-dy@users.noreply.github.com>
2019-10-13 00:46:31 -0700

    created ADA.adb

---
+++ languages/ADA.adb
@@ -0,0 +1,10 @@
+
+-- LANGUAGE: Ada
+-- AUTHOR: Goldy Thundiyil Joseph
+-- GITHUB: https://github.com/gol-dy
+
+with Ada.Text_IO; use Ada.Text_IO;
+procedure HelloWorld is
+begin
+   Put_Line ("Hello World!");
+end HelloWorld;
